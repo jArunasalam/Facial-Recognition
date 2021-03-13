38af1bb6bea237c9d20a95c6fdbfea9b6d24dc3e
adi78-1411 <73550114+adi78-1411@users.noreply.github.com>
2021-03-12 21:35:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
 import face_recognition
+from imutils import face_utils
+import dlib
 import os
 from datetime import datetime
+from scipy.spatial import distance as dist
 
 path = ((r"SAMPLE_FACIAL_DATASET"))
 images = []
@@ -14,6 +17,7 @@ for cl in myList:
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
 
+
 def findEncodings(images):
     encodeList = []
     for img in images:
@@ -21,6 +25,7 @@ def findEncodings(images):
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
+
 
 def markAttendance(name):
     with open('Attendence.csv', 'r+') as f:
@@ -34,8 +39,43 @@ def markAttendance(name):
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
 
+
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
+
+# p = our pre-treined model directory, on my case, it's on the same script's diretory.
+p = "shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(p)
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+
+def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+
+    # return the eye aspect ratio
+    return ear
+
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+
+# initialize the frame counters and the total number of blinks
+COUNTER = 0
+TOTAL = 0
+test = None
 
 cap = cv2.VideoCapture(0)
 
@@ -48,22 +88,90 @@ while True:
 
     facesCurFrame = face_recognition.face_locations(imgS)
     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+    # eyes related.
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Get faces into webcam's image
+    rects = detector(gray, 0)
+
+    # For each detected face, find the landmark.
+    for (i, rect) in enumerate(rects):
+        # Make the prediction and transfom it to numpy array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        ear = (leftEAR + rightEAR) / 2.0
+
+        # threshold, and if so, increment the blink frame counter
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+            test = False  # Mess with this variable to test eye detection********************************
+            print('COUNTER:', COUNTER)
+        # otherwise, the eye aspect ratio is not below the blink
+        # threshold
+        else:
+            # if the eyes were closed for a sufficient number of
+            # then increment the total number of blinks
+            if COUNTER >= EYE_AR_CONSEC_FRAMES and test == False:
+                TOTAL += 1
+                print('TOTAL:', TOTAL)
+                test = True  # Mess with this variable to test eye detection********************************
+                # reset the eye frame counter
+            # COUNTER = 0
+
+            # Draw on our image, all the finded cordinate points (x,y)
+        for (x, y) in shape:
+            cv2.circle(img, (x, y), 2, (0, 255, 0), -1)
 
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+
         # print(faceDis)
         matchIndex = np.argmin(faceDis)
-        if faceDis[matchIndex] < 0.50:
-            name = classNames[matchIndex].upper()
-            markAttendance(name)
+        if faceDis[matchIndex] < 0.50 and test == True:  # 50 is too low, raise this
+            name = classNames[matchIndex].upper()  # RECOGNIZED IN SYSTEM: COLOR IS GREEN
+            # markAttendance(name)
+            # recognized face
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        # print(name) --prints image name
+
+        elif faceDis[matchIndex] > 0.50 and test == True:
+            name = 'Detecting'  # not recognized in the system: LIVE HUMAN DETECTED (NOT RECOGNIZED IN SYSTEM)
+            # print('UNKNOWN HUMAN DETECTED') #--prints unknown face
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 255), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 255), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            COUNTER = 0
+            test == False  # Mess with this variable to test eye detection********************************
         else:
-            name = 'Unknown'
-        # print(name)
-        y1, x2, y2, x1 = faceLoc
-        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-cv2.imshow('Webcam', img)
-cv2.waitKey(1)
+            name = 'Unknown'  # not recognized in the system: COLOR IS RED
+            # print("NOT IN THE SYSTEM") #--prints unknown face and no detection of facial liveness
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 0, 255), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            test == False  # Mess with this variable to test eye detection********************************
+        # recognized face
+        # y1, x2, y2, x1 = faceLoc
+        # y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        # cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        # cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+    cv2.imshow('FACIAL RECOGNITION', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
